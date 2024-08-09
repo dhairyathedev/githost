5337159d0b9584dc7c512421a2476410f174d279
PriyanshuValiya <147643182+PriyanshuValiya@users.noreply.github.com>
2024-08-09 15:40:09 +0530

    add deploy page

---
+++ www/app/dashboard/page.tsx
@@ -0,0 +1,155 @@
+"use client";
+
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const formSchema = z.object({
+  gitrepo: z.string().min(1, {
+    message: "URL can't be empty.",
+  }),
+});
+
+function dashboard() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      gitrepo: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  const logs = [
+    {
+      time: "08:16:45",
+      text: "Finding your repo in GitHub"
+    },
+    {
+      time: "08:16:50",
+      text: "Mongoose Connection Successfull..."
+    },
+    {
+      time: "08:16:55",
+      text: "Backend Deployed"
+    },
+    {
+      time: "08:17:00",
+      text: "Checking all Environments Variables"
+    },
+    {
+      time: "08:17:10",
+      text: "Build Successfully..."
+    },
+    {
+      time: "08:16:15",
+      text: "Your Website is live at GitHost."
+    }
+  ];
+
+  return (
+    <div className="max-w-screen-md mx-auto m-2 p-4 w-full h-screen">
+      <h2 className="text-3xl font-semibold">
+        GitHost Dashboard
+      </h2>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex mt-10 gap-x-3"
+        >
+          <FormField
+            control={form.control}
+            name="gitrepo"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input
+                    className="w-full"
+                    type="url"
+                    placeholder="Enter Your Git-Repo Link..."
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Deploy</Button>
+        </form>
+      </Form>
+
+      <div className="mt-16">
+        <div className="text-2xl font-semibold">
+          Deployment Details
+        </div>
+      </div>
+
+      <div className="mt-4 w-11/12">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="text-lg pl-6">
+              Build Logs
+            </AccordionTrigger>
+            <AccordionContent>
+              {logs.map((ele) => (
+                <div className="ml-6 pt-3 font-mono">
+                  <p>{ele.time} : {ele.text}</p>
+                </div>
+              ))}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+
+      <div className="w-11/12">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="text-lg pl-6">
+              Deployment Summary
+            </AccordionTrigger>
+            <AccordionContent>
+              <div className="ml-6 pt-3 font-mono">Your Website : https://PriyanshuValiya-Google-notes.githost.xyz</div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+
+      <div className="w-11/12">
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="text-lg pl-6">
+              Assigning Custom Domians
+            </AccordionTrigger>
+            <AccordionContent>
+            <div className="ml-6 pt-3 font-mono">You Can Add Your Custom Domains. <u>Add Custom Domain</u></div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
+    </div>
+  );
+}
+
+export default dashboard;
--- www/app/login/page.tsx
+++ www/app/login/page.tsx
@@ -1,13 +1,48 @@
+"use client"
+
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export function ProfileForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+ 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+}
+
 
 function LoginPage() {
+
   return (
     <main className="container flex items-center justify-center h-screen w-4/3  ">
-      <form className="container w-1/3 h-[full] border-2 border-gray-300 my-10 mx-20 rounded-xl ">
+      {/* <form className="container w-1/3 h-[full] border-2 border-gray-300 my-10 mx-20 rounded-xl ">
         <h1 className="text-3xl font-medium px-3 my-3 py-3 w-96 text-center">
           Login Page
         </h1>
@@ -37,7 +72,29 @@ function LoginPage() {
         <Button className="my-7 font-semibold w-80 bg-black text-white px-24 mx-7 py-2 rounded-lg">
           Login
         </Button>
+      </form> */}
+
+      {/* <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
       </form>
+    </Form> */}
     </main>
   );
 }
